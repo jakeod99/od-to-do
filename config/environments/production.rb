2488354ed8ed039cb156bb57f8e92b99bc4af548
Jake O'Donnell <jakeod99@gmail.com>
2025-07-18 23:29:55 -0400

    add todo as a dns binding host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,8 @@ Rails.application.configure do
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
   #
+  config.hosts << "todo"
+
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
